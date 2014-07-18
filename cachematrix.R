036a58ce967e07d83dca7d4519b0560cec7ade43
C.C. Wang <kevinnewgame@hotmail.com>
2014-07-18 17:13:46 +0800

    I've done the MakeCacheMatrix part

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We assign function makeCacheMatrix and its input, say a matrix
+## to a symbol like this: "A <- makeCacheMatrix(X)". Then we can
+## set the inverse of "X" and save in A, set a new matrix to 
+## substitute "X", and get "X". After that we can check whether we
+## set the inverse of "X" in A by using function cacheSolve like
+## this: "cacheSolve(A)". If not, cacheSolve gives us a inverse of
+## X.
 
-## Write a short comment describing this function
+## Four function in A. They are
+## 1. getmatrix() # get the input of makeCacheMatrix, that is "X"
+## 2. setmatrix() # give a new matrix to substitute "X" in A
+## 3. setinverse() # give a matrix. We can calculate the inverse
+## of "X" and save it in A
+## 4. getinverse() # get the matrix we set
 
-makeCacheMatrix <- function(x = matrix()) {
+## We use them in this way: A$getmatrix(), A$setmatrix("your matrix")
+## For this assignment, assume that the matrix supplied is always invertible.
 
+MakeCacheMatrix <- function(x = numeric()){
+        inv <- NULL
+        setinverse <- function(y) inv <<- y
+        getinverse <- function() inv
+        
+        getmatrix <- function() x
+        setmatrix <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        list(setinverse = setinverse,
+             getinverse = getinverse,
+             getmatrix = getmatrix,
+             setmatrix = setmatrix)
 }
-
 
 ## Write a short comment describing this function
 
